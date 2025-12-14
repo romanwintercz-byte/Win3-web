4c19932dcd8baec33aa50dcd2c1aa904bcba901e
romanwintercz-byte <roman.winter.cz@gmail.com>
2025-12-14 13:31:31 +0100

    start

--- constants.tsx
+++ constants.tsx
@@ -4,7 +4,8 @@ import React from 'react';
 
 export const BRAND = {
   name: 'DevPortfolio',
-  logoUrl: '/logo.svg',
+  // Nyní aplikace hledá soubor logo.jpg ve složce public
+  logoUrl: '/logo.jpg',
 };
 
 export const PROJECTS: Project[] = [
